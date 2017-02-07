9adb378d4eb38cdbd22b3431c7d378604b3833c5
wnowak <wnowak10@gmail.com>
2017-02-07 10:23:03 -0500

    added data preparation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kaggle_nature_conserv
+
+steps
+
+0. prepare_files to begin building dataframe with file, label. 
+1. run anokas to find boat ID's (clustering ID) for each image
+2. use tensorflow inception to get features
+3. split train, validation set
+3. run xgboost on 2048 features, plus boat id. return prediction on validation
+4. run tensorflow classification. return prediction on validation
+5. create ensemble. combine weighted xgboost and tensorflow.
---
+++ prepare_files.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import os 
+
+
+# training images
+images_dir = '/Users/wnowak/gitt/kaggle_nature_conserv/train/'
+
+species=[images_dir+f for f in os.listdir(images_dir)[1:]]
+
+
+file_paths=[]
+fish_type=[]
+for i in range(len(species)):
+    for j in range(len(os.listdir(species[i]))):
+        images=os.listdir(species[i])
+        file_paths.append(species[i]+'/'+images[j])
+        fish_type.append(species[i][-3:])
+#         file_path.append(species[i]+os.listdir(species[j])
+
+print(file_paths[1:10])
+
+file_array=np.asarray(file_path)
+fish_array=np.asarray(fish_type)
+
+# combine into array for training
+train_array=np.c_[file_array,fish_array]
+
+# just get the files:
+
+train_array[:,0]
